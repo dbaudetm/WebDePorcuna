97e8ab249599ad58b561cc75e2e66c829c667ee9
dbaudetm <92870616+dbaudetm@users.noreply.github.com>
2021-11-11 22:14:30 +0100

    Eleven

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
-const input = document.querySelector("#input");
-const city = document.querySelector("#city");
-
-const cityName = document.querySelector("#cityName");
 const Temp = document.querySelector("#temp");
 const main = document.querySelector("#main");
-const discription = document.querySelector("#discription");
-const image = document.querySelector("#image");
+
 function weatherUpdate() {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${"Porcuna"}&appid=ded8fb24404261afff13e88870c2cf84&lang=sp`);
-    //xhr.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${"Porcuna"}&lang=es&appid=25040fd4d13ee481256615303a864d13`);
-
-    //api.openweathermap.org/data/2.5/weather?q=${"Porcuna"}&lang=es&appid=25040fd4d13ee481256615303a864d13
-    // in place of appid enter your open weather API Key
-    // You can create it for free
-    // https://home.openweathermap.org/users/sign_up
 
     http: xhr.send();
     xhr.onload = () => {
